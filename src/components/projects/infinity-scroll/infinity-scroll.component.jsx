b5871060958b53c48c8ac305da8128c6469e82f8
JOHN NASIMBA <nasimba4john@gmail.com>
2021-07-17 20:49:29 +0200

    Updating projects

--- src/components/projects/infinity-scroll/infinity-scroll.component.jsx
+++ src/components/projects/infinity-scroll/infinity-scroll.component.jsx
@@ -8,10 +8,10 @@ const InfinityScroll = () => {
            
             <div className="project-details">
                 <h4 className="project-title">
-                   LIGHT AND DARK MODE
+                  INFINITY SCROLL
                 </h4>
                 <p className="project-description">
-                The idea behind this website is to demonstrate how one can add light and dark mode themes commonly used these days.
+                The project demonstrates how infinity scrolling, commonly used on social media platforms, can be implemented using Javascript.
                 </p>
                 <div className="project-languages">
                     <img src="./images/icons/js.png" alt="JavaScript"  />
@@ -19,16 +19,16 @@ const InfinityScroll = () => {
                     <img src="./images/icons/css.png" alt="CSS"  />
                 </div>
                 <div className="project-links">
-                    <a href="https://johnnasimba.github.io/Light-Dark-mode/" target="_blank" rel="noopener noreferrer" className="primary-button">
+                    <a href="https://johnnasimba.github.io/Infinite-Scroll/" target="_blank" rel="noopener noreferrer" className="primary-button">
                        <span>View The Site</span>  
                     </a>
-                    <a href="https://github.com/Johnnasimba/Light-Dark-mode" target="_blank" rel="noopener noreferrer" className="secondary-button">
+                    <a href="https://github.com/Johnnasimba/Infinite-Scroll" target="_blank" rel="noopener noreferrer" className="secondary-button">
                         <span>Source Code</span> 
                     </a>
                 </div>
             </div>
             <div className="project-image">
-                <img src="./images/projects-illustrations/darkmode.png" alt="Light and Dark mode" />
+                <img src="./images/projects-illustrations/infinity-scroll.png" alt="Light and Dark mode" />
 
             </div>
         </div>
