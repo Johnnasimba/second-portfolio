334edf99d70c97593c8a6bffb0e1fd9e2d7663ef
JOHN NASIMBA <nasimba4john@gmail.com>
2021-08-18 15:56:46 +0200

    Converting to styled-components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,42 +1,114 @@
 import React from 'react';
-import './header.styles.css';
 import { ReactComponent as LogoIcon} from './logo.svg';
-
+import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
+
+const Wrapper = styled.div`
+    position: fixed;
+    width: 1100px;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    align-items: center;
+    justify-content: space-between;
+    height: 50px;
+    top: 12px;
+`;
+const Logo = styled(NavLink)`
+    width: 46px;
+    height: 42px;
+`;
+const NavLinksContainer = styled.ul`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-end;
+    list-style-type: none;
+`;
+const TabContainer = styled.li`
+    position: relative;
+    margin-right: 26px;
+    color: white ;
+    &:hover {
+        border-bottom: 2px solid #F94040;  
+    }
+`;
+
+const TabLink = styled(NavLink)`
+    font-size: 24px;
+    line-height: 28px;
+    text-decoration: none;
+    color: white ;  
+    &.active{
+        border-bottom: 2px solid #F94040;
+    }
+`;
+const  ContactLinkContainer = styled.li`
+    width: 103px;
+    height: 35px;
+    text-align: center;
+    margin: 0 auto;
+    border: 1px solid #FB5B57;
+    color: #FB5B57; 
+    overflow: hidden;  
+`;
+const ContactLink = styled(NavLink)`
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 24px;
+    line-height: 28px;
+    color: #FB5B57;
+
+    font-size: 24px;
+    line-height: 28px;
+    text-decoration: none;
+    &.active{
+        border-bottom: 2px solid #F94040;
+    }
+`;
+const MicroCredentialsLink = styled.a`
+    font-size: 24px;
+    line-height: 28px;
+    text-decoration: none;
+    color: white ;  
+`;
+
+
 
 const Header = () => {
     return (
-        <div className="header">
-            <NavLink className="logo" exact to="/">
+        <Wrapper>
+            <Logo exact to="/">
                 <LogoIcon />
-            </NavLink>
-            <ul className="navigation-tabs">
-                <li className="tab">
-                    <NavLink exact to="/">
+            </Logo>
+            <NavLinksContainer>
+                <TabContainer>
+                    <TabLink exact to="/">
                         Home
-                    </NavLink>
-                </li>
-                <li className="tab">
-                    <NavLink exact to="/about">
+                    </TabLink>
+                </TabContainer>
+                <TabContainer >
+                    <TabLink exact to="/about">
                         About
-                    </NavLink>
-                </li>
-                <li className="tab">
-                    <NavLink exact  to="/projects">
+                    </TabLink>
+                </TabContainer>
+                <TabContainer>
+                    <TabLink exact  to="/projects">
                         Projects
-                    </NavLink>
-                </li>
+                    </TabLink>
+                </TabContainer>
                
-                <li className="tab">
-                    <a href="https://micro-credentials.herokuapp.com/" target="_blank" rel="noopener noreferrer">Micro-credentials</a>
-                </li>
-                <li className="tab contact">
-                    <NavLink exact  to="/contact">
+                <TabContainer >
+                    <MicroCredentialsLink href="https://micro-credentials.herokuapp.com/" target="_blank" rel="noopener noreferrer">Micro-credentials</MicroCredentialsLink>
+                </TabContainer>
+                <ContactLinkContainer>
+                    <ContactLink exact  to="/contact">
                         Contact
-                    </NavLink>
-                </li>
-            </ul>
-        </div>
+                    </ContactLink>
+                </ContactLinkContainer>
+            </NavLinksContainer>
+        </Wrapper>
     )
 }
 
