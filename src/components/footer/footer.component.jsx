d92e979c63686f3f1c28f4795997995f365a6243
JOHN NASIMBA <nasimba4john@gmail.com>
2021-08-18 15:05:39 +0200

    Converting to styled-components

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,42 +1,126 @@
 import React from 'react';
-import './footer.styles.css';
+import styled from 'styled-components';
+
+const Wrapper = styled.footer`
+    width: 100vw;
+    height: 350px;
+    background: #1B3447;
+`;
+const Container = styled.div`
+    width: 270px; 
+    height: 210;
+    margin: 0 auto;
+
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    align-items: center;
+    justify-content: center;
+`;
+const Title = styled.h5`
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 36px;
+    line-height: 42px;
+    margin-bottom: 15px;
+
+    color: #FFFFFF;
+`;
+const Quote = styled.p`
+    padding: 0;
+    margin: 2px;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 14px;
+    text-align: center;
+
+    color: #FFFFFF;
+`;
+const SocialMediaIcons = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    align-items: center;
+    justify-content: space-between;
+
+    margin-top: 20px;
+`;
+const SocialMediaLink = styled.a`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    width: 40px;
+    height:40px;
+    margin-right: 10px;
+ 
+    color: #fff;
+    background: #06273D;
+    list-style: none;  
+    text-decoration: none;
+    border-radius: 50%;
+    border: 1px solid #FB5B57;
+`;
+const ClosingRemarks = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin-top: 10px;
+    margin-bottom: 0;
+`;
+const CopyRight = styled.p`
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 12px;
+    line-height: 14px;
+
+    color: #FFFFFF
+`;
+const OwnerName = styled.span`
+    font-weight: bold;
+`;
+
 
 const Footer = () => {
     return (
-        <footer className="footer">
-            <div className="footer-container">
-                <h5 className="my-name">JOHN NASIMBA</h5>
-                <p  className="quote">A program is never less than 90% complete</p>
-                <p  className="quote">and</p>
-                <p  className="quote">never more than 95% complete</p>
-                <div className="social-media-icons">
+        <Wrapper>
+            <Container>
+                <Title>JOHN NASIMBA</Title>
+                <Quote>A program is never less than 90% complete</Quote>
+                <Quote>and</Quote>
+                <Quote>never more than 95% complete</Quote>
+                <SocialMediaIcons>
                     {/* email */}
-                    <a href="http://" target="_blank" rel="noopener noreferrer">
+                    <SocialMediaLink href="mailto:nasimba4john@gmail.com" target="_blank" rel="noopener noreferrer">
                         <i className="far fa-envelope fa-lg"></i>
-                    </a>
+                    </SocialMediaLink>
                     {/*linkedIn*/}
-                    <a href="http://" target="_blank" rel="noopener noreferrer">
+                    <SocialMediaLink href="https://www.linkedin.com/in/john-nasimba/" target="_blank" rel="noopener noreferrer">
                         <i className="fab fa-linkedin-in fa-lg"></i>
-                    </a>
+                    </SocialMediaLink>
                     {/* facebook */}
-                    <a href="http://" target="_blank" rel="noopener noreferrer">
+                    <SocialMediaLink href="https://www.facebook.com/john.nasimba.7" target="_blank" rel="noopener noreferrer">
                         <i className="fab fa-facebook fa-lg"></i>
-                    </a>
+                    </SocialMediaLink>
                     {/* whatsApp */}
-                    <a href="http://" target="_blank" rel="noopener noreferrer">
+                    <SocialMediaLink href="https://wa.me/+27780387591" target="_blank" rel="noopener noreferrer">
                         <i className="fab fa-whatsapp fa-lg"></i>
-                    </a>
+                    </SocialMediaLink>
                     {/* Github */}
-                    <a href="http://" target="_blank" rel="noopener noreferrer">
+                    <SocialMediaLink href="https://github.com/Johnnasimba/" target="_blank" rel="noopener noreferrer">
                         <i className="fab fa-github fa-lg"></i>
-                    </a>
-                </div>
-                <div className="closing-marks">
-                    <i className="far fa-copyright"></i>
-                    <p>2020-21 <span className="footer-last-name">John Nasimba</span> | All Rights  Reserved</p>
-                </div>
-            </div>
-        </footer>
+                    </SocialMediaLink>
+                </SocialMediaIcons>
+                <ClosingRemarks>
+                    <i className="far fa-copyright" style={{marginRight:'10px', color: '#fff'}}></i>
+                    <CopyRight>2020-21 <OwnerName className="footer-last-name">John Nasimba</OwnerName> | All Rights  Reserved</CopyRight>
+                </ClosingRemarks>
+            </Container>
+        </Wrapper>
     )
 }
 
