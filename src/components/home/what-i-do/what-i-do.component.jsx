eadc06025185e1771ff55ed3169a4050ad51f956
JOHN NASIMBA <nasimba4john@gmail.com>
2021-08-18 21:01:30 +0200

    Converting to styled components

--- src/components/home/what-i-do/what-i-do.component.jsx
+++ src/components/home/what-i-do/what-i-do.component.jsx
@@ -1,30 +1,117 @@
 
 import React from 'react'
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
-import './what-i-do.styles.css';
+const Wrapper = styled.div`
+    width: 100vw;
+    height: 1000px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background: #06273D;
+`;
+const Container = styled.div`
+    width: 1100px;
+    height: 100%;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+const Title = styled.h2`
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 30px;
+    line-height: 42px;
+    margin-top: 200px;
+    color: #FFFFFF;
+`;
+const Cards = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+`;
+const Card = styled.div`
+    width: 300px;
+    height: 320px;
+    margin-right: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: center;    
+`;
+const WebDesignCard = styled(Card)`
+    background: linear-gradient(0deg, rgba(6, 39, 61, 0.4), rgba(6, 39, 61, 0.4)), url("/images/web-design.jpg");
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+`;
+const FrontEndCard = styled(Card)`
+    background: linear-gradient(0deg, rgba(27, 52, 71, 0.38), rgba(27, 52, 71, 0.38)), url("/images/front-end-development.jpg");
+    box-shadow: 5px 4px 4px rgba(0, 0, 0, 0.25);
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+`;
+const BackEndCard = styled(Card)`
+    background: linear-gradient(0deg, rgba(6, 39, 61, 0.69), rgba(6, 39, 61, 0.69)), url("/images//back-end-development.jpg");
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+`;
+const CardTitle = styled.h4`
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 28px;
+    color: #FFFFFF;
+`;
+const Button = styled.button`
+    width: 219px;
+    height: 66px;
+    margin-top: 200px;
+    background: #1B3447;
+    border: 4px solid #FB5B57;
+    box-sizing: border-box;
+    border-radius: 10px;    
+`;
+const NavLink = styled(Link)`
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 28px;
+    color: #FB5B57;
+    text-decoration: none;
+`;
 
 const WhatIDo = () => {
     return (
-        <div className="what-i-do-section">
-            <div className="what-i-do-container">
-                <h3>
-                    WHAT I DO
-                </h3>
-                <div className="cards ">
-                    <div className="card web-design">
-                        <h4>UX Design</h4>
-                    </div>
-                    <div className="card front-end">
-                        <h4>Front-End Development</h4>
-                    </div>
-                    <div className="card back-end">
-                        <h4>Back-End Development</h4>
-                    </div>
+        <Wrapper>
+            <Container>
+                <Title> WHAT I DO  </Title>
+                <Cards>
+                    <WebDesignCard>
+                        <CardTitle>UX Design</CardTitle>
+                    </WebDesignCard>
+                    <FrontEndCard>
+                        <CardTitle>Front-End Development</CardTitle>
+                    </FrontEndCard>
+                    <BackEndCard>
+                        <CardTitle>Back-End Development</CardTitle>
+                    </BackEndCard>
                 
-                </div>
-                <button>Download CV</button>  
-            </div>
-        </div>
+                </Cards>
+                <Button> 
+                    <NavLink to='/'>Download CV</NavLink>
+                </Button>  
+            </Container>
+        </Wrapper>
     )
 }
 
--- src/components/home/work-section/work-section.component.jsx
+++ src/components/home/work-section/work-section.component.jsx
@@ -1,23 +1,99 @@
 import React from 'react'
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-import './work-section.styles.css';
+const Wrapper = styled.div`
+    width: 100vw;
+    height: 963px;
+    background: #1B3447;
+
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    align-items: center;
+    justify-content: center;
+`;
+const Title = styled.h2`
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 30px;
+    line-height: 42px;
+    margin-top: 200px;
+    color: #FFFFFF;
+`;
+const Container = styled.div`
+    width: 1440px;
+    height: 600px;
+    display: flex;
+    justify-content: center;
+`;
+const ProjectDescription = styled.div`
+    width: 548px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    justify-content: flex-start;
+    margin-right: 40px;
+`;
+const Paragraph = styled.p`
+    width: 350px;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 40px;
+    margin-top: 0;
+    margin-right: 0;
+
+    color: #FFFFFF;
+`;
+const Button = styled.button`
+    width: 240px;
+    height: 62px;
+    margin-right: 105px;
+    margin-top: 27px;
+    background: #1B3447;
+    border: 4px solid #FB5B57;
+    box-sizing: border-box;
+    border-radius: 10px;    
+`;
+const NavLink = styled(Link)`
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 42px;
+    color: #FB5B57;
+    text-decoration: none;
+`;
+const Figure = styled.figure`
+    width: 548px;
+    height: 367px;
+`;
+const Image = styled.img`
+    height: 340px;
+`;
+
+
+
 
 const WorkSection = () => {
     return (
-        <div className="work-section">
-           <h3 className="work">WORK</h3>
-           <div className="work-description-and-image">
-               <div className="work-description-and-button">
-                   <p>I am always developing applications while constantly learning. I strongly believe in practice makes perfect. Check out some of the projects I have worked on. </p>
-                   <button>
-                       View Projects
-                   </button>
-               </div>
-               <figure>
-                   <img src="./images/work-image.png" alt="work environment" />
-               </figure>
-           </div>
-        </div>
+        <Wrapper>
+           <Title>PROJECTS</Title>
+           <Container>
+               <ProjectDescription>
+                   <Paragraph>I am always developing applications while constantly learning. I strongly believe in practice makes perfect. Check out some of the projects I have worked on. </Paragraph>
+                   <Button>
+                       <NavLink>View Projects</NavLink>                       
+                   </Button>
+               </ProjectDescription>
+               <Figure>
+                   <Image src="./images/work-image.png" alt="work environment" />
+               </Figure>
+           </Container>
+        </Wrapper>
     )
 }
 
